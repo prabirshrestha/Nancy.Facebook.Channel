3b8611d60992f5e31870d146e6c83332915c359a
prabirshrestha <mail@prabir.me>
2012-11-21 14:37:26 -0500

    implemented FacebookChannelResponse

--- src/Nancy.Facebook.Channel/FacebookChannelResponse.cs
+++ src/Nancy.Facebook.Channel/FacebookChannelResponse.cs
@@ -1,7 +1,22 @@
 ﻿
 namespace Nancy.Facebook.Channel
 {
-    public class FacebookChannelResponse
+    using System;
+    using System.Text;
+
+    public class FacebookChannelResponse : Response
     {
+        private static readonly byte[] ContentByteArray = Encoding.UTF8.GetBytes("<script src=\"//connect.facebook.net/en_US/all.js\"></script>");
+
+        public FacebookChannelResponse(int cacheExpires = 60 * 60 * 24 * 365)
+        {
+            Headers["Pragma"] = "public";
+            Headers["Cache-Control"] = "maxage=" + cacheExpires;
+            Headers["Expires"] = DateTime.Now.ToUniversalTime().AddSeconds(cacheExpires).ToString("r");
+
+            ContentType = "text/html";
+
+            Contents = stream => stream.Write(ContentByteArray, 0, ContentByteArray.Length);
+        }
     }
 }
